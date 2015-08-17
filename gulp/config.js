a09a965258c0a0180095117e4b79205b8174de78
classicmike <michael.tran.tech@gmail.com>
2015-08-17 21:47:58 +1000

    Cannot figure out why cannot browserify

--- gulp/config.js
+++ gulp/config.js
@@ -1,18 +1,18 @@
-var destination = '.build';
-var src = '.src';
+var destination = './build';
+var src = './src';
 
 module.exports = {
     browserify: {
         debug: true,
         bundleConfigs: {
-            src: './src/js/*.js',
-            dest: './build/js/',
+            src: ['./src/js/*.js'],
+            dest: destination + '/js/',
             sourceDestinationFile: 'main.js'
         }
     },
     jshint : {
         config: {
-            src: './src/js/*.js'
+            src: src + '/js/*.js'
         }
     }
 };
--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -1,24 +1,20 @@
 var gulp = require('gulp');
-var browserify = require('gulp-browserify');
-var uglify = require('uglify');
+var browserify = require('browserify');
+var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
+var transform = require('vinyl-transform');
 
 var browserifyConfig = require('../config').browserify;
 
 //browserify task
 gulp.task('browserify', function(){
-    var browserified = transform(function(fileName){
-        var b = browserify(fileName);
-        return b.bundle();
-    });
-
-    return gulp.src(browserifyConfig.src)
-        .pipe(browserified)
-        .pipe(browserifyConfig.sourceDestinationFile)
-        .pipe(uglify())
-        .pipe(gulp.dest(browserifyConfig.dest));
+    // Browserify/bundle the css
+    browserify(browserifyConfig.bundleConfigs.src)
+        .bundle()
+        .pipe(source(browserifyConfig.bundleConfigs.sourceDestinationFile))
+        .pipe(gulp.dest(browserifyConfig.bundleConfigs.dest));
 });
 
