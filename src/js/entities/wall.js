1f87c76c89f0979b875d3f509784ee1d98796e87
classicmike <michael.tran.tech@gmail.com>
2015-09-03 08:32:24 +1000

    About to add a main wall class need to think on how inheritance works in this context

---
+++ src/js/entities/wall.js
@@ -0,0 +1,14 @@
+var physicsComponent = require('../components/physics/physics');
+var collisionComponent = require('../components/collision/rect');
+
+Wall = function(bus){
+    this.setup(bus);
+};
+
+Wall.prototype.setup = function(bus){
+};
+
+Wall.prototype.setEvents = function(){
+
+};
+
