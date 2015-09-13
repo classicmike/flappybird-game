9a517dd85a9a73e4a9d84f6e1f045d2a6602fd0e
classicmike <michael.tran.tech@gmail.com>
2015-09-13 21:33:20 +1000

    Updated

--- js/main.js
+++ js/main.js
@@ -425,7 +425,13 @@ exports.RectCollisionComponent = RectCollisionComponent;
 },{}],4:[function(require,module,exports){
 var BirdGraphicsComponent = function(entity){
     this.entity = entity;
+    this.imgObject = new Image();
+    this.imgObject.src = BirdGraphicsComponent.BIRD_IMG_SRC;
+    this.imgObject.onload = this.onload.bind(this);
 };
+
+
+//remember the width of the image in terms of pixel size is 32pixels
 
 BirdGraphicsComponent.prototype.draw = function(context){
     var position = this.entity.components.physics.position;
@@ -433,12 +439,16 @@ BirdGraphicsComponent.prototype.draw = function(context){
     context.save();
     context.translate(position.x, position.y);
     context.beginPath();
-    context.arc(0, 0, 0.02, 0, 2*Math.PI);
-    context.fill();
+    context.arc(0, 0, BirdGraphicsComponent.BIRD_RADIUS, 0, 2*Math.PI);
     context.closePath();
+    context.drawImage(this.imgObject, 2*BirdGraphicsComponent.BIRD_RADIUS, 2*BirdGraphicsComponent.BIRD_RADIUS);
     context.restore();
-
 };
+
+
+
+BirdGraphicsComponent.BIRD_IMG_SRC = './img/fb_bird.png';
+BirdGraphicsComponent.BIRD_RADIUS = 0.02;
 
 
 exports.BirdGraphicsComponent = BirdGraphicsComponent;
